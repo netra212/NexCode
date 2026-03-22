78e1103e19235e1846ea984ce6281e60aeaa78c9
netra212 <netra200021kcbdr@gmail.com>
2026-03-22 19:11:36 +0000

    main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const main = require("./config/db");
+const cookieParser = require("cookie-parser");
 
-app.listen(process.env.PORT, () => {
-  console.log("Server Listening at port number: " + process.env.PORT);
-});
+// req.body ko data JSON format ma huncha so needs to into Javascript Object.
+
+app.use(express.json());
+app.use(cookieParser);
+
+main()
+  .then(async () => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server Listening at port number: " + process.env.PORT);
+    });
+  })
+  .catch((err) => console.log("Error Occured: " + err));
