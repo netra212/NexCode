76f77cf7a153d0a7f6f106c787a175f54385e4e7
netra212 <netra200021kcbdr@gmail.com>
2026-03-23 17:41:07 +0000

    fix: missing parenthesis in cookieParser & added redisClient

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ const app = express();
 require("dotenv").config();
 const main = require("./config/db");
 const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/userAuth");
+const redisClient = require("./config/redis");
 
 // req.body ko data JSON format ma huncha so needs to into Javascript Object.
 
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
-main()
-  .then(async () => {
+app.use("/user", authRouter);
+
+const InitializedConnection = async (req, res) => {
+  try {
+    await Promise.all([main(), redisClient.connect()]);
+    console.log("DB Connected");
+
     app.listen(process.env.PORT, () => {
       console.log("Server Listening at port number: " + process.env.PORT);
     });
-  })
-  .catch((err) => console.log("Error Occured: " + err));
+  } catch (err) {
+    console.log("Error: " + err);
+  }
+};
+
+InitializedConnection();
