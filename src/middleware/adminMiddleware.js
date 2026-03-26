aeb84b83e2d4b56a88e6ad761d2e219fe90500cf
netra212 <netra200021kcbdr@gmail.com>
2026-03-26 11:34:42 +0000

    feat: adminMiddleware

---
+++ src/middleware/adminMiddleware.js
@@ -0,0 +1,46 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const redisClient = require("../config/redis");
+
+const adminMiddleware = async (req, res, next) => {
+  try {
+    // First fetch the token from cookies.
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Tokens is not present.");
+    }
+    const payload = await jwt.verify(token, process.env.JWT_KEY);
+
+    const { _id } = payload;
+
+    if (!_id) {
+      throw new Error("Invalid token");
+    }
+
+    const result = await User.findById(_id);
+
+    // Check either token is admin or not.
+    if (payload.role != "admin") {
+      throw new Error("Invalid token");
+    }
+
+    if (!result) {
+      throw new Error("User does not exist.");
+    }
+
+    // Check if this is present into the blocklist of redis or not.
+    const IsBlocked = await redisClient.exists(`token:${token}`);
+
+    if (IsBlocked) {
+      throw new Error("Invalid Token");
+    }
+
+    req.result = result;
+
+    next();
+  } catch (err) {
+    res.status(401).send("Error: " + err.message);
+  }
+};
+
+module.exports = adminMiddleware;
