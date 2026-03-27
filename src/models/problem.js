9270d72d2a36c124aec608fc642ed00b77231de5
netra212 <netra200021kcbdr@gmail.com>
2026-03-27 18:58:31 +0000

    feat: problem schema

--- src/models/problem.js
+++ src/models/problem.js
@@ -68,6 +68,18 @@ const problemSchema = new Schema({
       },
     },
   ],
+  referenceSolution: [
+    {
+      language: {
+        type: String,
+        required: true,
+      },
+      completeCode: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   problemCreator: {
     type: Schema.Types.ObjectId,
     ref: "user",
