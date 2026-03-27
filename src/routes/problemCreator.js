09cd2017aa0d1f0e6f8483f102538ba30021e976
netra212 <netra200021kcbdr@gmail.com>
2026-03-27 18:56:58 +0000

    feat: implementing problem creator

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,18 +1,33 @@
 const express = require("express");
+const adminMiddleware = require("../middleware/adminMiddleware");
+const {
+  CreateProblem,
+  UpdateProblem,
+  deleteProblem,
+  getProblemById,
+  getAllProblem,
+} = require("../Controllers/userProblem");
+const userMiddleware = require("../middleware/userMiddleware");
 const problemRouter = express.Router();
 
 // Create: creating a problem., admin access need.
-problemRouter.post("/create", problemCreate);
+problemRouter.post("/create", adminMiddleware, CreateProblem);
 
 // Update: admin access need.
-problemRouter.patch("/:id", problemUpdate);
+problemRouter.patch("/update/:id", adminMiddleware, UpdateProblem);
 
 // Delete: admin access need.
-problemRouter.delete("/:id", problemDelete);
+problemRouter.delete("/delete/:id", adminMiddleware, deleteProblem);
 
 // Fetch: fetching all problem.,
-problemRouter.get("/:id", problemFetch);
-problemRouter.get("/", problemFetchAll);
+problemRouter.get("/problemByiD/:id", userMiddleware, getProblemById);
+problemRouter.get("/getAllProblem", userMiddleware, getAllProblem);
 
 // problem solved.
-problemRouter.get("/user", problemSolved);
+problemRouter.get(
+  "/problemSolvedByUser",
+  userMiddleware,
+  getAllSolvedProblemByUser,
+);
+
+module.exports = problemRouter;
