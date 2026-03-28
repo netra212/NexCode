e0eaf7f82709824cc4ec92cd356f2a9f0c7b611c
netra212 <netra200021kcbdr@gmail.com>
2026-03-28 23:17:46 +0000

    feat: submit code features

---
+++ src/routes/submit.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const userMiddleware = require("../middleware/userMiddleware");
+const submitCode = require("../Controllers/submitCode");
+const submitRouter = express.Router();
+
+submitRouter.post("/submit/:id", userMiddleware, submitCode);
