23b5a296bf99eeb1d2209d7e213fe103e0f58084
netra212 <netra200021kcbdr@gmail.com>
2026-03-30 18:54:46 +0100

    initial commit: finish one and delete

--- src/models/user.js
+++ src/models/user.js
@@ -1,51 +1,62 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 20,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 20,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      immutable: true,
+    },
+    age: {
+      type: Number,
+      min: 6,
+      max: 80,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    problemSolved: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "problem",
+        },
+      ],
+      unique: true,
+      timestamps: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    minLength: 3,
-    maxLength: 20,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    immutable: true,
-  },
-  age: {
-    type: Number,
-    min: 6,
-    max: 80,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  problemSolved: {
-    type: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "problem",
-      },
-    ],
-    unique: true,
+  {
     timestamps: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+);
 
-const User = mongoose.model("user", userSchema);
-module.exports = User;
+// const User = mongoose.model("user", userSchema);
+// module.exports = User;
+
+userSchema.post("findOneAndDelete", async function (userInfo) {
+  if (userInfo) {
+    await mongoose.model("submission").deleteMany({ userId: userInfo._id });
+  }
+});
